1d9156693b85a09bf62bc8e4fca9e8d58a4311b4
Aliza Sharma <aliza.sharma12@gmail.com>
2023-07-06 22:27:25 -0500

    added get item to local storage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
   let currentDay = dayjs().format("dddd, MMMM D, YYYY")
   $("#currentDay").text(currentDay)
 
-  // fetch the hour and hold that in a variable, then set background color 
   let currentHour = dayjs().hour()
 
   $('.time-block').each(function(){
@@ -19,7 +18,6 @@ $(document).ready(function () {
     }
   })
   
-
   $('.btn').on("click", function(){
     let key = $(this).parent().attr('id')
     let value = $(this).siblings('textarea').val()
@@ -27,17 +25,11 @@ $(document).ready(function () {
     localStorage.setItem(key, value)
   })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
-  localStorage.getItem('hour-9')
+  localStorage.getItem('name')
 
-
+console.log(localStorage)
 
 });
 
 
-  //
-  //
-  //
+  
