a84bc73924ce1c88881b6c86c7cbc58451b3a069
Aliza Sharma <aliza.sharma12@gmail.com>
2023-07-06 21:56:53 -0500

    added javascript elemets and jquery

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,43 @@
-$(document).ready(function() {
-  var t = moment().format("MMMM Do YYYY");
-  let e = document.getElementById("currentDay");
-  e.innerHTML = t;
-  var a = moment().format("HH");
-  $("#clearFieldsBtn").click(function(t) {
-      t.preventDefault,
-      $("textarea").val(""),
-      localStorage.clear()
-  }),
-
-  $(".time-div").each(function() {
-      var t = $(this).attr("id").split("-")[1];
-      a == t ? ($(this).addClass("present"),
-      $(this).children(".description").addClass("white-text")) : a < t ? ($(this).removeClass("present"),
-      $(this).addClass("future")) : t < a && ($(this).removeClass("future"),
-      $(this).addClass("past"))
-  }),
-
-  $(".saveBtn").click(function(t) {
-      t.preventDefault();
-      var e = $(this).siblings(".time-block").val()
-        , t = $(this).parent().attr("id").split("-")[1];
-      localStorage.setItem(t, e)
-  }),
+$(document).ready(function () {
   
-  $("#hour-09 .time-block").val(localStorage.getItem("09")),
-  $("#hour-10 .time-block").val(localStorage.getItem("10")),
-  $("#hour-11 .time-block").val(localStorage.getItem("11")),
-  $("#hour-12 .time-block").val(localStorage.getItem("12")),
-  $("#hour-13 .time-block").val(localStorage.getItem("13")),
-  $("#hour-14 .time-block").val(localStorage.getItem("14")),
-  $("#hour-15 .time-block").val(localStorage.getItem("15")),
-  $("#hour-16 .time-block").val(localStorage.getItem("16")),
-  $("#hour-17 .time-block").val(localStorage.getItem("17"))
-});
+  let currentDay = dayjs().format("dddd, MMMM D, YYYY")
+  $("#currentDay").text(currentDay)
+
+  // fetch the hour and hold that in a variable, then set background color 
+  let currentHour = dayjs().hour()
+
+  $('.time-block').each(function(){
+    let rowHour = parseInt($(this).attr('id').split('-')[1])
  
+    if(currentHour > rowHour){
+      $(this).addClass('past')
+    } else if(currentHour === rowHour){
+      $(this).addClass('present')
+    }else{ 
+      $(this).addClass('future')
+
+    }
+  })
+  
+
+  $('.btn').on("click", function(){
+    let key = $(this).parent().attr('id')
+    let value = $(this).siblings('textarea').val()
+
+    localStorage.setItem(key, value)
+  })
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  
+  localStorage.getItem('hour-9')
+
+
+
+});
+
+
+  //
+  //
+  //
